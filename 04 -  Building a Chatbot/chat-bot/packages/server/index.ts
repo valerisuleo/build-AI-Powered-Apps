20f763379ab638ba12a31b2924342cfc16fcbf2e
Valerio Risuleo <valerio.risuleo@gmail.com>
2026-03-23 18:43:52 +0000

    section 4: Creating the OpenAI client

--- 04 -  Building a Chatbot/chat-bot/packages/server/index.ts
+++ 04 -  Building a Chatbot/chat-bot/packages/server/index.ts
@@ -1,19 +1,59 @@
-import dotenv from "dotenv";
+import dotenv from 'dotenv';
 dotenv.config();
 
-import express, { type Request, type Response } from "express";
+import express, { type Request, type Response } from 'express';
+import OpenAI from 'openai';
+import z from 'zod';
 
 const app = express();
 const port = process.env.PORT || 3000;
+app.use(express.json());
 
-app.get("/", (request: Request, response: Response) => {
-  response.send("Hello World!!!");
+const client = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY,
 });
 
-app.get("/api/hello", (request: Request, response: Response) => {
-  response.json({ message: "ok, lets play the Beatles" });
+const conversations = new Map<string, string>();
+const chatSchema = z.object({
+    prompt: z
+        .string()
+        .trim()
+        .min(1, 'Prompt is required')
+        .max(1000, 'Prompt is too long. Max 1000 characters'),
+    conversationId: z.uuid('Invalid UUID'),
+});
+
+app.post('/api/chat', async (request: Request, response: Response) => {
+    const parseResult = chatSchema.safeParse(request.body);
+
+    if (!parseResult.success) {
+        response.status(400).json(parseResult.error.format());
+        return;
+    }
+
+    try {
+        const { prompt, conversationId } = parseResult.data;
+
+        const result = await client.responses.create({
+            model: 'gpt-4o-mini',
+            input: prompt,
+            temperature: 0.2,
+            max_output_tokens: 100,
+            previous_response_id: conversations.get(conversationId),
+        });
+
+        conversations.set(conversationId, result.id);
+
+        response.json({
+            message: result.output_text,
+        });
+    } catch (error) {
+        response.status(500).json({
+            error: 'Failed to generate a response',
+        });
+    }
 });
 
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+    console.log(`Server is running on http://localhost:${port}`);
 });
